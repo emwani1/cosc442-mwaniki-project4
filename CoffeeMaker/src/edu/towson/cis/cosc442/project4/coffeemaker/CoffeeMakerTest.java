48c34f63c00f3a2c2da2a2afe22ab10fddca9223
emwani1 <edwinmwaniki@Edwins-MBP-2.home>
2017-03-19 23:22:07 -0400

    Coffee Maker Unit Test

--- CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
@@ -1,42 +1,100 @@
 package edu.towson.cis.cosc442.project4.coffeemaker;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-/**
- *
- */
-public class CoffeeMakerTest extends TestCase {
-	private CoffeeMaker cm;
-	private Inventory i;
-	private Recipe r1;
+import org.junit.Test;
 
-	public void setUp() {
+public class CoffeeMakerTest {
+	CoffeeMaker cm ;
+	Recipe r;
+	Recipe r2;
+	public void setUp(){
 		cm = new CoffeeMaker();
-		i = cm.checkInventory();
-
-		r1 = new Recipe();
-		r1.setName("Coffee");
-		r1.setPrice(50);
-		r1.setAmtCoffee(6);
-		r1.setAmtMilk(1);
-		r1.setAmtSugar(1);
-		r1.setAmtChocolate(0);
+		r = new Recipe();
+		r2 = new Recipe();
+		
+		r.setName("Choclate");
+		r.setAmtChocolate(1);
+		r.setAmtCoffee(0);
+		r.setAmtMilk(1);
+		r.setAmtSugar(1);
+		r.setPrice(10);
+		
+		r2.setName("Coffee");
+		r2.setAmtChocolate(0);
+		r2.setAmtCoffee(1);
+		r2.setAmtMilk(1);
+		r2.setAmtSugar(1);
+		r2.setPrice(20);
+	
 	}
 
-	public void testAddRecipe1() {
-		assertTrue(cm.addRecipe(r1));
+	@Test
+	public void testAddRecipe() {
+		setUp();
+		assertEquals(true,cm.addRecipe(r));
+		assertEquals(true,cm.addRecipe(r2));
 	}
 
-	public void testDeleteRecipe1() {
-		cm.addRecipe(r1);
-		assertTrue(cm.deleteRecipe(r1));
+	@Test
+	public void testDeleteRecipe() {
+		setUp();
+		cm.addRecipe(r);
+		cm.addRecipe(r2);
+		assertEquals(true,cm.deleteRecipe(r));
+		assertEquals(true,cm.deleteRecipe(r2));
+		Recipe r3 = new Recipe();
+		assertEquals(false,cm.deleteRecipe(r3));
 	}
 
-	public void testEditRecipe1() {
-		cm.addRecipe(r1);
-		Recipe newRecipe = new Recipe();
-		newRecipe = r1;
-		newRecipe.setAmtSugar(2);
-		assertTrue(cm.editRecipe(r1, newRecipe));
+	@Test
+	public void testEditRecipe() {
+		setUp();
+		cm.addRecipe(r);
+		cm.addRecipe(r2);
+		Recipe r3 = new Recipe();
+		
+		r3.setName("Latte");
+		r3.setAmtChocolate(0);
+		r3.setAmtCoffee(1);
+		r3.setAmtMilk(1);
+		r3.setAmtSugar(1);
+		r3.setPrice(30);
+		assertEquals(true,cm.editRecipe(r3, r));
+		assertEquals(true,cm.editRecipe(r3, r2));
+		assertEquals(false,cm.editRecipe(r, r3));
 	}
+
+	@Test
+	public void testAddInventory() {
+		setUp();
+		int coffee = 1;
+		int milk = 1;
+		int sugar = 0;
+		int choclate = 1;
+		assertEquals(true,cm.addInventory(coffee, milk, sugar, choclate));
+		sugar = 1;
+		assertEquals(false,cm.addInventory(coffee, milk, sugar, choclate));
+		
+	}
+
+	@Test
+	public void testMakeCoffee() {
+		setUp();
+		cm.addRecipe(r);
+		cm.addRecipe(r2);
+		assertEquals(0,cm.makeCoffee(r2, 20),0.001);
+		assertEquals(40,cm.makeCoffee(r, 50),0.001);
+	}
+
+
+	@Test
+	public void testGetRecipeForName() {
+		setUp();
+		cm.addRecipe(r);
+		cm.addRecipe(r2);
+		assertEquals(r,cm.getRecipeForName(r.getName()));
+		assertEquals(r2,cm.getRecipeForName(r2.getName()));
+	}
+
 }
